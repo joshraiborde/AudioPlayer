79765686dffe8a1c816dade71a17c2a45723e149
joshraiborde <joshraiborde@gmail.com>
2021-10-30 13:07:23 -0400

    added showDuration function

--- js/main.js
+++ js/main.js
@@ -57,3 +57,25 @@ $('#stop').click(function(){
 	$('#play').show();
     $('#duration').fadeOut(400);
 });
+
+
+// time duration
+function showDuration() {
+    $(audio).bind('timeupdate', function(){
+        // get hours and minutes
+        var s = parseInt(audio.currentTime % 60);
+        var m = parseInt((audio.currentTime) / 60) % 60;
+
+        //  add 0 if less thean 10
+        if(a < 10){
+            s = '0' + s;
+        }
+        $('#duration')/html(m + '.' + s);
+        var value = 0;
+        if(audio.currentTime > 0){
+            value = Math.floor((100 / audio.duration) * audio.currentTime);
+        }
+
+        $()
+    })
+}
