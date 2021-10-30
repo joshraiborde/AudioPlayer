ca7f738501ff84be8672820369335ee8de03c8a0
joshraiborde <joshraiborde@gmail.com>
2021-10-30 13:48:56 -0400

    troubleshooting player

--- js/main.js
+++ js/main.js
@@ -1,110 +1,115 @@
 var audio;
 
 // hide the pause button
-
 $('#pause').hide();
-
-// initalize audio
+	
+// plays the first song
 initAudio($('#playlist li:first-child'));
-
-// initializer function
+	
 function initAudio(element){
 	var song = element.attr('song');
     var title = element.text();
     var cover = element.attr('cover');
     var artist = element.attr('artist');
 
-    // create audio object
-    audio = new Audio('media/' + song);
-
-    if(!audio.currentTime){
+	// new audio object is created
+	audio = new Audio('media/' + song);
+	
+	if(!audio.currentTime){
 		$('#duration').html('0.00');
 	}
 
-    $('#audio-player .title').text(title);
+	$('#audio-player .title').text(title);
     $('#audio-player .artist').text(artist);
-
-    // insert the cover image
-
-    $('img.cover').attr('src','images/covers/' + cover);
-    
-
-    $('#playlist li').removeClass('active');
-    element.addClass('active')
+	
+	// cover image is inserted
+	$('img.cover').attr('src','images/covers/' + cover);
+	
+	$('#playlist li').removeClass('active');
+    element.addClass('active');
 }
 
-// playbutton
+
+// playButton
 $('#play').click(function(){
-    audio.play();
+	audio.play();
 	$('#play').hide();
 	$('#pause').show();
 	$('#duration').fadeIn(400);
 	showDuration();
 });
 
-// pausebutton
+// pauseButton
 $('#pause').click(function(){
-    audio.pause();
+	audio.pause();
 	$('#pause').hide();
 	$('#play').show();
 });
-
-// stopbuttton
+	
+// stopButton
 $('#stop').click(function(){
-    audio.pause();
-    audio.currentTime = 0;
+	audio.pause();		
+	audio.currentTime = 0;
 	$('#pause').hide();
 	$('#play').show();
-    $('#duration').fadeOut(400);
+	$('#duration').fadeOut(400);
 });
 
-// nextbuttton
+// nextButton
 $('#next').click(function(){
     audio.pause();
-    var next = $('#playlist li.active').next;
-    if(next.length == 0){
+    var next = $('#playlist li.active').next();
+    if (next.length == 0) {
         next = $('#playlist li:first-child');
     }
     initAudio(next);
-    audio.play();
-    showDuration();
+	audio.play();
+	showDuration();
 });
 
-// prevbuttton
+// prevButton
 $('#prev').click(function(){
     audio.pause();
-    var prev = $('#playlist li.active').prev;
-    if(prev.length == 0){
+    var prev = $('#playlist li.active').prev();
+    if (prev.length == 0) {
         prev = $('#playlist li:last-child');
     }
     initAudio(prev);
-    audio.play();
-    showDuration();
+	audio.play();
+	showDuration();
 });
 
-// volume button
+// playlistClick
+$('#playlist li').click(function () {
+    audio.pause();
+    initAudio($(this));
+	$('#play').hide();
+	$('#pause').show();
+	$('#duration').fadeIn(400);
+	audio.play();
+	showDuration();
+});
+
+// volumeButton
 $('#volume').change(function(){
-    audio.volume = parseFloat(this.value / 10);
-})
-
-
-// time duration
-function showDuration() {
-    $(audio).bind('timeupdate', function(){
-        // get hours and minutes
-        var s = parseInt(audio.currentTime % 60);
-        var m = parseInt((audio.currentTime) / 60) % 60;
-
-        //  add 0 if less thean 10
-        if(s < 10){
-            s = '0' + s;
-        }
-        $('#duration')/html(m + '.' + s);
-        var value = 0;
-        if(audio.currentTime > 0){
-            value = Math.floor((100 / audio.duration) * audio.currentTime);
-        }
-
-        $('#progress').css('width', value+'%');
-    })
+	audio.volume = parseFloat(this.value / 10);
+});
+	
+// timeDuration
+function showDuration(){
+	$(audio).bind('timeupdate', function(){
+		//Get hours and minutes
+		var s = parseInt(audio.currentTime % 60);
+		var m = parseInt((audio.currentTime / 60) % 60);
+		//Add 0 if seconds less than 10
+		if (s < 10) {
+			s = '0' + s;
+		}
+		$('#duration').html(m + '.' + s);	
+		var value = 0;
+		if (audio.currentTime > 0) {
+			value = Math.floor((100 / audio.duration) * audio.currentTime);
+		}
+		$('#progress').css('width',value+'%');
+	});
 }
