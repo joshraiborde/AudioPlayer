57b9a87d409bfc47fb7064d60b8d8338f4392df4
joshraiborde <joshraiborde@gmail.com>
2021-10-30 12:49:35 -0400

    fixed typo for images location

--- js/main.js
+++ js/main.js
@@ -2,27 +2,32 @@ var audio;
 
 // hide the pause button
 
-$('#pause').hide()
+$('#pause').hide();
 
-function initAudio(element) {
-    var song = element.attr('song');
+// initalize audio
+initAudio($('#playlist li:first-child'));
+
+// initializer function
+function initAudio(element){
+	var song = element.attr('song');
     var title = element.text();
     var cover = element.attr('cover');
-    var artist = elment.attr('artist');
+    var artist = element.attr('artist');
 
     // create audio object
     audio = new Audio('media/' + song);
 
     if(!audio.currentTime){
-        $('#duration').html('0.00');
-    }
+		$('#duration').html('0.00');
+	}
 
     $('#audio-player .title').text(title);
     $('#audio-player .artist').text(artist);
 
     // insert the cover image
 
-    ${'img.cover'}.attr('src','img/covers/'+ cover);
+    $('img.cover').attr('src','images/covers/' + cover);
+    
 
     $('#playlist li').removeClass('active');
     element.addClass('active')
