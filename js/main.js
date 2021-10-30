c2024f8dd03a3e4e77a43ff15788b80342ff2623
joshraiborde <joshraiborde@gmail.com>
2021-10-30 12:39:22 -0400

    added feature to show title, artist, cover of a song

--- js/main.js
+++ js/main.js
@@ -10,6 +10,17 @@ function initAudio(element) {
     var cover = element.attr('cover');
     var artist = elment.attr('artist');
 
+    
+    audio = new Audio('media/' + song);
 
+    if(!audio.currentTime){
+        $('#duration').html('0.00');
+    }
 
+    $('#audio-player .title').text(title);
+    $('#audio-player .artist').text(artist);
+
+    
+
+    ${'img.cover'}.attr('src','img/covers/'+ cover);
 }
