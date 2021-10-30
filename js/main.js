b532d644b1c1e531fed14118111d507d092758ba
joshraiborde <joshraiborde@gmail.com>
2021-10-30 12:32:55 -0400

    added feature to hide pause button, added function to initAudio, added vars for song, title, cover and artist

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var audio;
+
+
+
+$('#pause').hide()
+
+function initAudio(element) {
+    var song = element.attr('song');
+    var title = element.text();
+    var cover = element.attr('cover');
+    var artist = elment.attr('artist');
+
+
+
+}
